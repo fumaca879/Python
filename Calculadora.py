d58f99f49aedf3f0a098d301727f8f5f7fa6632d
Davi Barbosa do Nascimento <fumaca879@gmail.com>
2025-10-22 23:37:35 -0300

    PrimeiroCommit

---
+++ Calculadora.py
@@ -0,0 +1,19 @@
+numero1 = float(input("Digite o primeiro Número: "))
+operacao = input("Digite a operação (+, -, * ou /): ")
+numero2 = float(input("Digite o segundo Número: "))
+
+
+if operacao == "+":
+    resultado = numero1 + numero2
+    print(resultado)
+elif operacao == "-":
+    resultado = numero1 - numero2
+    print(resultado)
+elif operacao == "*":
+    resultado = numero1 * numero2
+    print(resultado)
+elif operacao == "/":
+    resultado = numero1 / numero2
+    print(resultado)
+else:
+    print("Operação invalida")
